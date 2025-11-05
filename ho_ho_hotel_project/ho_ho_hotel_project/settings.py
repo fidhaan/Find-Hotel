e35c271ebef962c125ba79c1ef53001c87e68c5c
Fidhaan Muhammad <fiiidhaaan@gmail.com>
2025-11-05 18:11:28 +0530

    PA: Add STATIC_ROOT, ALLOWED_HOSTS, and other deployment settings

--- ho_ho_hotel_project/ho_ho_hotel_project/settings.py
+++ ho_ho_hotel_project/ho_ho_hotel_project/settings.py
@@ -25,12 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'projecthotel204.pythonanywhere.com',
     '.pythonanywhere.com',
-    'http://127.0.0.1:8000'
 ]
 
 
@@ -167,7 +166,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'login_app.CustomUser'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
@@ -175,7 +174,7 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
