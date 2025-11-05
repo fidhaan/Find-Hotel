b4130c36538d889515c529368e4b8116846268ec
Fidhaan Muhammad <fiiidhaaan@gmail.com>
2025-11-05 08:50:05 +0530

    Configure Django for MySQL on PythonAnywhere

--- ho_ho_hotel_project/ho_ho_hotel_project/settings.py
+++ ho_ho_hotel_project/ho_ho_hotel_project/settings.py
@@ -82,10 +82,20 @@ WSGI_APPLICATION = 'ho_ho_hotel_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('NAME'), # Use the full name from PythonAnywhere
+        'USER': config('USER'),              # Your PythonAnywhere username
+        'PASSWORD': config('PASSWORD'),    # The password you set on the Databases tab
+        'HOST': config('HOST')             # The Hostname from PythonAnywhere
     }
 }
 
